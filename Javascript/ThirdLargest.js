c6e7fbcd2639021e18e6faee0cb4516ca4fdfb20
Deckins <bennythai2013@hotmail.com>
2019-11-19 20:32:17 -0500

    Modified runtime from n^2 to n

--- Javascript/ThirdLargest.js
+++ Javascript/ThirdLargest.js
@@ -7,32 +7,29 @@
  * https://leetcode.com/problems/third-maximum-number/submissions/
  */
 var thirdMax = function(nums) {
-      
-    nums.sort((a,b)=>{
-        return a-b;
-    });
     let uniqueSet = new Set(nums)
     nums = [...uniqueSet];
     let biggest = nums[0];
-    let secBiggest = 0
-    let thirdBiggest = 0
+    let secBiggest = null
+    let thirdBiggest = null
   if(nums.length < 3){
       return Math.max(...nums);
   } else{
       for(let i =1; i<nums.length;i++){
-          console.log(nums[i]);
           if(biggest < nums[i]){
+              if(secBiggest != null)
               thirdBiggest = secBiggest;
               secBiggest = biggest;
               biggest = nums[i];
           } 
-          // else if(secBiggest < nums[i]){
-          //     thirdBiggest=secBiggest;
-          //     secBiggest=nums[i];
-          // } else{
-          //     thirdBiggest = nums[i];
-          // }
+          else if(secBiggest < nums[i]){
+              thirdBiggest=secBiggest;
+              secBiggest=nums[i];
+          } else if(thirdBiggest == null || thirdBiggest < nums[i]){
+              thirdBiggest = nums[i];
+          }
       }
   }
   return thirdBiggest;
 };
+console.log(thirdMax([0,1,-2147483648,2,2,1,-399999]));
