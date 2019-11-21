cab368737b990e19e48596e40b34f0c3b7b37194
Deckins <bennythai2013@hotmail.com>
2019-11-21 15:10:11 -0500

    Sum of all Nodes in Binary Tree

---
+++ Javascript/BinaryTreeSum.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} L
+ * @param {number} R
+ * @return {number}
+ */
+var rangeSumBST = function(root,L,R) {
+    let sum =0;
+    let leftSum = 0;
+    let rightSum = 0;
+    if(root == null){
+        return 0;
+    } else {
+    
+//         leftSum = rangeSumBST(root.left,L,R)
+//         rightSum = rangeSumBST(root.right,L,R)
+        
+//         if(root.val >= L && root.val <=R){
+//             sum = root.val + leftSum + rightSum
+//         }
+        sum += (((root.val >= L && root.val <= R) ? root.val : 0) + rangeSumBST(root.left,L,R) + rangeSumBST(root.right,L,R));
+        
+        return sum;
+    }
+};
