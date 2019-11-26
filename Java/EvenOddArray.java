496fdd1fbd6a0c62238d1be577175ea82aa6ab50
Deckins <bennythai2013@hotmail.com>
2019-11-26 11:54:45 -0500

    Runtime O(n) with all test cases

--- Java/EvenOddArray.java
+++ Java/EvenOddArray.java
@@ -1,29 +1,17 @@
+//https://leetcode.com/problems/sort-array-by-parity/solution/
+
 class Solution {
     public int[] sortArrayByParity(int[] A) {
-   
-        int swapPos = 0;
-        int temp = 0;
-        int oddCount = 0;
-        boolean swap = true;
-        for(int i =0; i<A.length;i++){
+        int[] newArr = new int[A.length];
+        int start =0;
+        int end = A.length -1 ;;
+        for(int i = 0; i< A.length; i++){
             if(A[i] % 2 == 0){
-                temp = A[swapPos];
-                A[swapPos] = A[i];
-                A[i] = temp;
-                if(oddCount > 0){
-                    swapPos = swapPos+1;
-                    oddCount--;
-                } else {
-                    swapPos = i;
-                }
-                swap = false;
-            } else if(swap == false)  {
-                swapPos = i;
-                swap = true;
+                newArr[start++] = A[i];
             } else {
-                oddCount++;
+                newArr[end--] = A[i];
             }
         }
-        return A;
-    }
+        return newArr;
+    }   
 }
