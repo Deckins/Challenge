a1e869d90f71464025dc808909f3c245bd34259e
Deckins <bennythai2013@hotmail.com>
2019-12-17 18:49:11 -0500

    Updated each recursive call stack to return list

--- DataStructures/InOrder.java
+++ DataStructures/InOrder.java
@@ -1,40 +1,34 @@
 /**
  * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/
  */
 class InOrder {
+    //Instance variable for global
     List<Integer> list = new ArrayList<Integer>();
-
     public List<Integer> inorderTraversal(TreeNode root) {
         // List<Integer> list = new ArrayList<Integer>();
+        // this.dfs(root,list);
+        // return list;
+
         if(root==null){
-            return list;
-            // dfs(root,list);
-            // return list;
-            // inorderTraversal(root.left);
-            // System.out.println(root.val);
-            // list.add(root.val);
-            // inorderTraversal(root.right);
-           
+            //Doesnt matter what is returned here because we are not
+            //keeping track of the return value for the function call
+            return new ArrayList<Integer>();
         }
         inorderTraversal(root.left);
-            System.out.println(root.val);
-            list.add(root.val);
-            return inorderTraversal(root.right);
-       
-        // return list;
+        System.out.println(root.val);
+        list.add(root.val);
+        inorderTraversal(root.right);
+        //returns all the traversed nodes for each recursive call stack
+        return list;
     }
-    public void dfs(TreeNode node, List<Integer> list){
-        if(node !=null){
-            dfs(node.left,list);
-            list.add(node.val);
-            dfs(node.right,list);
-        }
+    //Helper method to append each node to list Inorder
+    // public void dfs(TreeNode node, List<Integer> list){
+    //     if(node !=null){
+    //         dfs(node.left,list);
+    //         list.add(node.val);
+    //         dfs(node.right,list);
+    //     }
        
-    }
+    // }
 }
