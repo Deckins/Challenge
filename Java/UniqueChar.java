baa13a2faf1d8b280a2c495958b430eaf24d07db
Deckins <bennythai2013@hotmail.com>
2020-01-16 23:51:20 -0500

    HashMap Impl O(3n)

--- Java/UniqueChar.java
+++ Java/UniqueChar.java
@@ -1,26 +1,34 @@
-public class UniqueChar {
-
-    public static void main (String[] args){
-        System.out.println(firstUniqChar("dddccdbbaafffffi"));
-    }
-    public static int firstUniqChar(String s) {
-        boolean matched = false;
-        if(s.length() == 1){
-            return 0;
+class Solution {
+    int ret = 0;
+    public int firstUniqChar(String s) {
+        LinkedHashMap<Character,Integer> map = new LinkedHashMap<>();
+        List<Character> list = new ArrayList<>();
+        for(int i =0; i< s.length();i++){
+            if(map.containsKey(s.charAt(i)) == false){
+                map.put(s.charAt(i),1);
+            } else {
+                map.put(s.charAt(i),map.get(s.charAt(i))+1);
+            }
         }
+        map.forEach((k,v) -> {
+            System.out.println(k + " " +v);
+            if(v == 1){
+                // this.ret = k;
+                list.add(k);
+            }
 
+        });
+        if(list.isEmpty()) return -1;
+        int index = 0;
+        Character found = list.get(0);
         for(int i = 0; i<s.length();i++){
-            int notMatchCount= 0;
-            for(int j = 0;j<s.length();j++){
-                if(s.charAt(i) != s.charAt(j)){
-                    notMatchCount++;
-                }
+            if(found == s.charAt(i)){
+                index = i;
+                return index;
             }
-            if(notMatchCount == s.length() - 1){
-                return i;
-            }
+           
         }
-        return -1;
+        return index;
+      
     }
-
 }
