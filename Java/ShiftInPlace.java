1fb8dfbe3a24431bde08fb0303d8d8aba32dad34
Deckins <bennythai2013@hotmail.com>
2020-04-04 20:06:07 -0400

    O(N^2) run time for in place shifting

---
+++ Java/ShiftInPlace.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        for(int i = 0; i< nums.length; i++){
+            if(nums[i] == 0){
+                for(int j = 0; j<nums.length-1; j++){
+                    if(nums[j] == 0){
+                        
+                        nums[j] = nums[j+1];
+                        nums[j+1] = 0;      
+                    }
+                      
+                }
+                
+            }
+        }
+        
+    }
+}
