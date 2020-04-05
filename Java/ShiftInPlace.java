545cf96b0925215c168e2d3a712531dd15293e9a
Deckins <bennythai2013@hotmail.com>
2020-04-04 20:06:59 -0400

    Reduced runtime by half by accounting for adjacent zeros

--- Java/ShiftInPlace.java
+++ Java/ShiftInPlace.java
@@ -1,18 +1,20 @@
 class Solution {
     public void moveZeroes(int[] nums) {
         for(int i = 0; i< nums.length; i++){
+            int index = 0;
             if(nums[i] == 0){
-                for(int j = 0; j<nums.length-1; j++){
+                index = i;
+                for(int j = 0; j< nums.length-1; j++){
                     if(nums[j] == 0){
-                        
-                        nums[j] = nums[j+1];
-                        nums[j+1] = 0;      
+                        while(nums[j+1] == 0 && j<nums.length-2){
+                          j++;  
+                        }
+                        nums[index] = nums[j+1];
+                        nums[j+1] = 0;
+                        index++;
                     }
-                      
                 }
-                
             }
         }
-        
     }
 }
